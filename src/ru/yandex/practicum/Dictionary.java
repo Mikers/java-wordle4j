d4282cce474fe86cbffe497135bcbdc2e9297081
kuker <wormhole.slider@gmail.com>
2025-11-27 21:12:25 +0300

    Add missing newline separators in Dictionary interface for readability

--- src/ru/yandex/practicum/Dictionary.java
+++ src/ru/yandex/practicum/Dictionary.java
@@ -5,8 +5,11 @@ import java.util.Set;
 
 public interface Dictionary {
     boolean contains(String word);
+
     String getRandomWord();
+
     int size();
+
     Dictionary filterByHints(Map<Integer, Character> correctPositions,
                             Set<Character> presentLetters,
                             Set<Character> absentLetters);
